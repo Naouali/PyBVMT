d123770064fbfaca015bddbf1b41a967081e26bb
Naouali <elnaoualinebil@gmail.com>
2020-05-22 11:59:44 +0100

    dependancies added : succes

--- setup.py
+++ setup.py
@@ -14,12 +14,21 @@ setup(
     author="NAOUALI Nebil",
     author_email="elnaoualinebil@gmail.com",
     description="Mapi api module",
-    py_modules = ['fetchapi', 'mapi'],
+    py_modules = ['fetchapi', 'mapi'], 
     package_dir = {'':'src'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires = [
+        'pandas>=1.0.0',
+        'requests>= 2.20.0',
+        ],
+    extras_require = {
+        'dev': [
+            'pandas',
+            'requests']
+        },
     python_requires='>=3.6',
     )
